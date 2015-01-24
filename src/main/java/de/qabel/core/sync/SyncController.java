c98a584051243acc0ed4d02d6da5b2621658d1aa
Alexander Bothe <info@alexanderbothe.com>
2015-01-24 21:33:41 +0100

    First model of how to possibly implement Sync.

---
+++ src/main/java/de/qabel/core/sync/SyncController.java
@@ -0,0 +1,51 @@
+package de.qabel.core.sync;
+
+import de.qabel.core.config.StorageServer;
+import de.qabel.core.config.StorageVolume;
+import de.qabel.core.drop.DropController;
+import de.qabel.core.drop.DropURL;
+import de.qabel.core.storage.StorageController;
+
+public class SyncController {
+
+	StorageVolume syncStorageVolume;
+	DropURL notificationDrop;
+
+	long lastSyncAttempt;
+
+	public SyncController(DropController dropController, StorageController storageController, StorageServer storageServer, StorageVolume syncStorageVolume, DropURL drop) {
+		this.syncStorageVolume = syncStorageVolume;
+		this.notificationDrop = drop;
+
+        // Register DropCallback in DropController:
+		//      if SyncDropMessage arriving, doSync() gets invoked. 
+		// Register ackack actor:
+		//      wait 5 minutes
+		//      invoke doSync()
+		// Hook in a doSync()-invoke somewhere before Qabel starts to shuts down.
+	}
+
+	public void doSync() {
+        // Compare Date.getTime() - lastSyncAttempt > e.g. 30 seconds to prevent sync spams.
+
+        // Get settings-managing objects //TODO: Which and how can these be reached with no singletons?
+		
+		// Generate local sync data JSON
+		// Acquire header of syncStorageVolume
+		// Check whether it's changed over time, i.e. via comparing last-modified against lastSyncAttempt or hash comparison against the http-delivered content checksum
+		// If stuff changed, acquire entire JSON from storage.
+		
+		// JSON merging:
+		// compare each SyncSettingsItem with the recently downloaded one, take the newer. discard older parts
+		// Deploy changed SyncSettingsItems to runtime.
+		
+		// If no local SyncSettingsItem was newer than a remote one:
+		// return;
+		
+		// Generate local sync data JSON
+		// Upload it to given syncStorageVolume
+
+		// Push drop message that a sync has been done
+	}
+
+}
---
+++ src/main/java/de/qabel/core/sync/SyncDropMessage.java
@@ -0,0 +1,6 @@
+package de.qabel.core.sync;
+
+import de.qabel.core.drop.ModelObject;
+
+public class SyncDropMessage extends ModelObject {
+}
